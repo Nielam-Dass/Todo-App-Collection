6ea20f2ba732cdc281dfd06bb8ebff999916e7f2
Nielam Dass <nielam.dass@gmail.com>
2025-02-26 23:04:18 -0500

    Store task list with useState hook

--- todo_apps/001-react-ts-app/src/App.tsx
+++ todo_apps/001-react-ts-app/src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react"
 import AddTodoForm from "./AddTodoForm"
 import TodoList from "./TodoList"
 
 function App() {
+  const [tasks, setTasks] = useState<string[]>(["Task 1", "Task 2", "Task 3", "Task 4"]);
+
   return (
     <>
       <h1>Todo App</h1>
       <AddTodoForm />
-      <TodoList/>
+      <TodoList tasks={tasks}/>
     </>
   )
 }
--- todo_apps/001-react-ts-app/src/TodoList.tsx
+++ todo_apps/001-react-ts-app/src/TodoList.tsx
@@ -1,13 +1,15 @@
 import { ReactNode } from "react";
 import TodoItem from "./TodoItem";
 
-function TodoList(){
-    const tasks: string[] = ["Task 1", "Task 2", "Task 3"];
+interface TodoListProps {
+    tasks: string[];
+}
 
+function TodoList(props: TodoListProps){
     return (
         <>
         <ul>
-            {tasks.map((task: string, index: number): ReactNode => {
+            {props.tasks.map((task: string, index: number): ReactNode => {
                 return(
                     <li key={index}>
                         <TodoItem taskName={task}/>
