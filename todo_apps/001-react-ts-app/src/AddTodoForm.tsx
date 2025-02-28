ac47cd52220688348ab6385efe42dafb1381f446
Nielam Dass <nielam.dass@gmail.com>
2025-02-28 00:18:53 -0500

    Store todo-urgency input value as React state

--- todo_apps/001-react-ts-app/src/AddTodoForm.tsx
+++ todo_apps/001-react-ts-app/src/AddTodoForm.tsx
@@ -2,10 +2,17 @@ import React, { useState } from "react"
 
 function AddTodoForm() {
     const [todoName, setTodoName] = useState<string>("");
+    const [todoUrgency, setTodoUrgency] = useState<number>(1);
     
-    const handleTodoNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleTodoNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         setTodoName((): string => {
             return event.target.value;
+        });
+    }
+
+    const handleTodoUrgencyChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setTodoUrgency((): number => {
+            return parseInt(event.target.value);
         });
     }
 
@@ -15,7 +22,7 @@ function AddTodoForm() {
             <input type="text" onChange={handleTodoNameChange} value={todoName} className="todo-name-input" id="todo-name"/>
             <br></br>
             <label htmlFor="todo-urgency">Task Urgency Level (1-10): </label>
-            <input type="number" min="1" max="10" className="todo-urgency-input" id="todo-urgency"/>
+            <input type="number" onChange={handleTodoUrgencyChange} value={todoUrgency} min="1" max="10" className="todo-urgency-input" id="todo-urgency"/>
             <br></br>
             <button>Add Todo</button>
         </>
