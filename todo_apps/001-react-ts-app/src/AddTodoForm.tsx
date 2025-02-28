f54584a4e85f4a63621b942c11aed1541cbb2c3b
Nielam Dass <nielam.dass@gmail.com>
2025-02-27 23:22:02 -0500

    Store todo-name input value as React state

--- todo_apps/001-react-ts-app/src/AddTodoForm.tsx
+++ todo_apps/001-react-ts-app/src/AddTodoForm.tsx
@@ -1,8 +1,18 @@
+import React, { useState } from "react"
+
 function AddTodoForm() {
+    const [todoName, setTodoName] = useState<string>("");
+    
+    const handleTodoNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTodoName((): string => {
+            return event.target.value;
+        });
+    }
+
     return (
         <>
             <label htmlFor="todo-name">Task Name: </label>
-            <input type="text" className="todo-name-input" id="todo-name"/>
+            <input type="text" onChange={handleTodoNameChange} value={todoName} className="todo-name-input" id="todo-name"/>
             <br></br>
             <label htmlFor="todo-urgency">Task Urgency Level (1-10): </label>
             <input type="number" min="1" max="10" className="todo-urgency-input" id="todo-urgency"/>
