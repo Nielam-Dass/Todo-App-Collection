a9a03b61620ae70712e24cda82cc03fbdd231d0e
Nielam Dass <nielam.dass@gmail.com>
2025-03-21 15:48:49 -0400

    Moved App component code into TodoHome component

--- todo_apps/001-react-ts-app/src/App.tsx
+++ todo_apps/001-react-ts-app/src/App.tsx
@@ -1,26 +1,11 @@
-import { JSX, useState } from "react";
-import AddTodoForm from "./AddTodoForm";
-import TodoTable from "./TodoTable";
-import Task from "./Task";
+import { JSX } from "react";
+import TodoHome from "./TodoHome";
 
 function App(): JSX.Element {
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  function addTask(newTask: Task): void{
-    setTasks((t: Task[]): Task[] => {
-      return [...t, newTask];
-    });
-  }
-
-  function deleteTask(index: number): void{
-    setTasks((t: Task[]): Task[] => t.filter((_element: Task, idx: number) => idx!==index));
-  }
-
+  
   return (
     <>
-      <h1>Todo App</h1>
-      <AddTodoForm addTodo={addTask}/>
-      <TodoTable tasks={tasks} deleteTodo={deleteTask}/>
+      <TodoHome/>
     </>
   )
 }
