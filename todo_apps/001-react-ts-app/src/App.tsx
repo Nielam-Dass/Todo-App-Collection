5a2c94fd6730b04fb7b037373e0de10a82aeb731
Nielam Dass <nielam.dass@gmail.com>
2025-03-22 00:32:43 -0400

    Added routes in App component

--- todo_apps/001-react-ts-app/src/App.tsx
+++ todo_apps/001-react-ts-app/src/App.tsx
@@ -1,11 +1,15 @@
 import { JSX } from "react";
 import TodoHome from "./TodoHome";
+import { Route, Routes } from "react-router-dom";
 
 function App(): JSX.Element {
   
   return (
     <>
-      <TodoHome/>
+      <Routes>
+        <Route path="/" element={<TodoHome />}/>
+        <Route path="/edit" element={<h1>Edit page</h1>}/>
+      </Routes>
     </>
   )
 }
