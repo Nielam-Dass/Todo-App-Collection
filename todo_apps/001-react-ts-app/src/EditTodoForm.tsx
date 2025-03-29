e82844d62cde10c11e7359d2ea6c89bd0d8e462a
Nielam Dass <nielam.dass@gmail.com>
2025-03-28 22:30:01 -0400

    Created edit form in EditTodoForm component

--- todo_apps/001-react-ts-app/src/EditTodoForm.tsx
+++ todo_apps/001-react-ts-app/src/EditTodoForm.tsx
@@ -1,12 +1,53 @@
-import { JSX } from "react";
+import React, { JSX, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 function EditTodoForm(): JSX.Element {
-    const params = useParams()
+    // TODO: Create common UI form component to use in EditTodoForm and AddTodoForm
+    const params = useParams();
+    const [todoName, setTodoName] = useState<string>("");
+    const [todoUrgency, setTodoUrgency] = useState<string>("");
+
+    const handleTodoNameChange: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>): void => {
+            setTodoName((): string => {
+                return event.target.value;
+            });
+        }
+    
+    const handleTodoUrgencyChange: React.ChangeEventHandler<HTMLInputElement> = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setTodoUrgency((tu: string): string => {
+            const urgencyLevel: number = parseInt(event.target.value);
+            if(Number.isNaN(urgencyLevel)) {
+                return "";
+            }
+            else if(urgencyLevel>=1 && urgencyLevel<=10) {
+                return urgencyLevel.toString();
+            }
+            return tu;
+        });
+    }
+
+    const handleEdit: React.MouseEventHandler<HTMLButtonElement> = (): void => {
+        throw new Error("Edit function not implemented yet");
+    }
+
+    const handleCancel: React.MouseEventHandler<HTMLButtonElement> = (): void => {
+        throw new Error("Cancel function not implemented yet");
+    }
+    
+
     return (
         <>
             <h1>Edit todo form for task at index {params.id}</h1>
             <Link to={"/"}>Home</Link>
+            <br></br>
+            <label htmlFor="todo-name">Task Name: </label>
+            <input type="text" onChange={handleTodoNameChange} value={todoName} className="todo-name-input" id="todo-name"/>
+            <br></br>
+            <label htmlFor="todo-urgency">Task Urgency Level (1-10): </label>
+            <input type="number" onChange={handleTodoUrgencyChange} value={todoUrgency} min="1" max="10" className="todo-urgency-input" id="todo-urgency"/>
+            <br></br>
+            <button onClick={handleEdit}>Edit</button>
+            <button onClick={handleCancel}>Cancel</button>
         </>
     )
 }
