9f50cfb739bdf1f5672ecab2782842bde199a285
Nielam Dass <nielam.dass@gmail.com>
2025-04-11 22:33:06 -0400

    Switch to using MemoryRouter instead of BrowserRouter in TodoHome component tests

--- todo_apps/001-react-ts-app/src/__tests__/TodoHome.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/TodoHome.test.tsx
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
 import TodoHome from '../TodoHome';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 
 describe("Todo home page tests", () => {
     it("Renders the title", () => {
         render(
-            <BrowserRouter>
+            <MemoryRouter>
                 <TodoHome tasks={[]} addTask={()=>{}} deleteTask={()=>{}}/>
-            </BrowserRouter>
+            </MemoryRouter>
         );
         expect(screen.getByText("Todo App")).toBeInTheDocument();
     });
