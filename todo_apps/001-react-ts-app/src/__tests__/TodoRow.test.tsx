8812cffcb713470bc22a71eb2eda111099df8931
Nielam Dass <nielam.dass@gmail.com>
2025-05-06 01:28:58 -0400

    Added tests for TodoRow component to ensure that onEdit and onRemove functions are called appropriately

--- todo_apps/001-react-ts-app/src/__tests__/TodoRow.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/TodoRow.test.tsx
@@ -1,5 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/vitest';
 
 import TodoRow from '../TodoRow';
@@ -11,5 +12,32 @@ describe("TodoRow component tests", () => {
         expect(screen.queryByText("123")).not.toBeInTheDocument();
         expect(screen.queryByRole("button", {name: "Edit"})).toBeInTheDocument();
         expect(screen.queryByRole("button", {name: "Remove"})).toBeInTheDocument();
+    });
+
+    it("Calls the onEdit function when button is clicked", async () => {
+        const handleEditMock: ()=>void = vi.fn();
+        const handleRemoveMock: ()=>void = vi.fn();
+        render(<TodoRow task={{taskId: "123", taskName:"Important task", taskUrgency: 4}} onEdit={handleEditMock} onRemove={handleRemoveMock}/>);
+        const editButton: HTMLElement = screen.getByRole("button", {name: "Edit"});
+
+        expect(handleEditMock).toHaveBeenCalledTimes(0);
+        expect(handleRemoveMock).toHaveBeenCalledTimes(0);
+        await userEvent.click(editButton);
+        expect(handleEditMock).toHaveBeenCalledTimes(1);
+        expect(handleRemoveMock).toHaveBeenCalledTimes(0);
+
+    });
+
+    it("Calls the onRemove function when button is clicked", async () => {
+        const handleEditMock: ()=>void = vi.fn();
+        const handleRemoveMock: ()=>void = vi.fn();
+        render(<TodoRow task={{taskId: "123", taskName:"Important task", taskUrgency: 4}} onEdit={handleEditMock} onRemove={handleRemoveMock}/>);
+        const removeButton: HTMLElement = screen.getByRole("button", {name: "Remove"});
+        
+        expect(handleEditMock).toHaveBeenCalledTimes(0);
+        expect(handleRemoveMock).toHaveBeenCalledTimes(0);
+        await userEvent.click(removeButton);
+        expect(handleEditMock).toHaveBeenCalledTimes(0);
+        expect(handleRemoveMock).toHaveBeenCalledTimes(1);
     });
 });
