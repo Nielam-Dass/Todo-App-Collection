274a30c5c79be452b9e99b91b8adc00be40f25da
Nielam Dass <nielam.dass@gmail.com>
2025-05-15 02:26:50 -0400

    Added test for displaying multiple todos in TodoTable component

--- todo_apps/001-react-ts-app/src/__tests__/TodoTable.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/TodoTable.test.tsx
@@ -1,10 +1,11 @@
 import { describe, expect, it, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom/vitest"
+import "@testing-library/jest-dom/vitest";
 
 import { MemoryRouter } from "react-router-dom";
 import TodoTable from "../TodoTable";
+import Task from "../Task";
 
 
 describe("TodoTable component tests", () => {
@@ -34,6 +35,30 @@ describe("TodoTable component tests", () => {
 
         expect(screen.queryByRole("table")).not.toBeInTheDocument();
         expect(screen.getByText("There are no tasks left")).toBeInTheDocument();
+    });
+
+    it("Renders multiple todos in table", () => {
+        const todos: Task[] = [
+            {taskId: "1", taskName: "My task", taskUrgency: 4},
+            {taskId: "2", taskName: "My other task", taskUrgency: 7},
+            {taskId: "3", taskName: "My final task", taskUrgency: 5}
+        ]
+        render(
+            <MemoryRouter>
+                <TodoTable tasks={todos} deleteTodo={()=>{}}/>
+            </MemoryRouter>
+        );
+        
+        const todoRows: HTMLElement[] = screen.queryAllByRole("row");
+        expect(todoRows).toHaveLength(4);
+        expect(todoRows[0]).toHaveTextContent("Task Name");
+        expect(todoRows[0]).toHaveTextContent("Urgency Level");
+        expect(todoRows[0]).toHaveTextContent("Edit Task");
+        expect(todoRows[0]).toHaveTextContent("Remove Task");
+
+        expect(todoRows[1]).toHaveTextContent("My task");
+        expect(todoRows[2]).toHaveTextContent("My other task");
+        expect(todoRows[3]).toHaveTextContent("My final task");
     });
 
     it("Calls the deleteTodo function when button is clicked", async () => {
