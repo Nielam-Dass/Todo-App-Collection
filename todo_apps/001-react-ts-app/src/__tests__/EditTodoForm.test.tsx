5bef91b9fd71353ae2137878eda185fead6ff86e
Nielam Dass <nielam.dass@gmail.com>
2025-05-27 00:28:37 -0400

    Created EditTodoFormWrapper component to simplify test rendering

--- todo_apps/001-react-ts-app/src/__tests__/EditTodoForm.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/EditTodoForm.test.tsx
@@ -2,19 +2,24 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
-import EditTodoForm from "../EditTodoForm";
 import { Route, Routes, StaticRouter } from "react-router-dom";
+import { JSX } from "react";
+import EditTodoForm from "../EditTodoForm";
+import Task from "../Task";
+
+function EditTodoFormWrapper({tasks, routeLocation, editTodo=()=>{}}: {tasks: Task[], routeLocation: string, editTodo?: (todo: Task)=>void}): JSX.Element {
+    return (
+        <StaticRouter location={routeLocation} >
+            <Routes>
+                <Route path="/edit/:id" element={<EditTodoForm tasks={tasks} editTodo={editTodo}/>}/>
+            </Routes>
+        </StaticRouter>
+    );
+}
 
 describe("EditTodoForm component tests", () => {
     it("Renders the edit form", () => {
-        render(
-            <StaticRouter location={"/edit/123"} >
-                <Routes>
-                    <Route path="/edit/:id" element={<EditTodoForm tasks={[{taskId: "123", taskName: "My task", taskUrgency: 4}]} editTodo={()=>{}}/>}/>
-                </Routes>
-            </StaticRouter>
-            
-        );
+        render(<EditTodoFormWrapper tasks={[{taskId: "123", taskName: "My task", taskUrgency: 4}]} routeLocation="/edit/123"/>);
         expect(screen.getByText("Edit Todo")).toBeInTheDocument();
         expect(screen.getByLabelText("Task Name:")).toBeInTheDocument();
         expect(screen.getByLabelText("Task Urgency Level (1-10):")).toBeInTheDocument();
