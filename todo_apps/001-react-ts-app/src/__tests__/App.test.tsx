80c76b3b62bd4edf8f833a5e478d757168d0a1c1
Nielam Dass <nielam.dass@gmail.com>
2025-06-10 00:40:46 -0400

    Added test for App component to ensure remove buttons delete associated todos

--- todo_apps/001-react-ts-app/src/__tests__/App.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/App.test.tsx
@@ -103,4 +103,64 @@ describe("App component tests", () => {
         expect(todoRows[2]).toHaveTextContent("Extra task");
         expect(todoRows[3]).toHaveTextContent("Another task");
     });
+
+    it("Removes todos when the associated remove buttons are clicked", async () => {
+        render(
+            <StaticRouter location={'/'}>
+                <App/>
+            </StaticRouter>
+        );
+        expect(screen.queryByText("There are no tasks left")).toBeInTheDocument();
+        expect(screen.queryByRole("table")).not.toBeInTheDocument();
+
+        const addButton: HTMLElement = screen.getByRole("button", {name: "Add Todo"});
+        const todoNameInputField: HTMLElement = screen.getByLabelText("Task Name:");
+        const todoUrgencyInputField: HTMLElement = screen.getByLabelText("Task Urgency Level (1-10):");
+
+        await userEvent.type(todoNameInputField, "My task");
+        await userEvent.type(todoUrgencyInputField, "5");
+        await userEvent.click(addButton);
+
+        await userEvent.type(todoNameInputField, "My other task");
+        await userEvent.type(todoUrgencyInputField, "8");
+        await userEvent.click(addButton);
+
+        await userEvent.type(todoNameInputField, "Another task");
+        await userEvent.type(todoUrgencyInputField, "3");
+        await userEvent.click(addButton);
+
+        await userEvent.type(todoNameInputField, "Extra task");
+        await userEvent.type(todoUrgencyInputField, "4");
+        await userEvent.click(addButton);
+
+        let todoRows: HTMLElement[] = screen.queryAllByRole("row").slice(1);
+        expect(todoRows).toHaveLength(4);
+
+        expect(todoRows[0]).toHaveTextContent("My other task");
+        expect(todoRows[1]).toHaveTextContent("My task");
+        expect(todoRows[2]).toHaveTextContent("Extra task");
+        expect(todoRows[3]).toHaveTextContent("Another task");
+
+        const removeButtons: HTMLElement[] = screen.queryAllByRole("button", {name: "Remove"});
+        
+        await userEvent.click(removeButtons[0]);
+
+        todoRows = screen.queryAllByRole("row").slice(1);
+        expect(todoRows).toHaveLength(3);
+
+        expect(screen.queryByText("My other task")).not.toBeInTheDocument();
+        expect(todoRows[0]).toHaveTextContent("My task");
+        expect(todoRows[1]).toHaveTextContent("Extra task");
+        expect(todoRows[2]).toHaveTextContent("Another task");
+
+        await userEvent.click(removeButtons[2]);
+
+        todoRows = screen.queryAllByRole("row").slice(1);
+        expect(todoRows).toHaveLength(2);
+
+        expect(screen.queryByText("My other task")).not.toBeInTheDocument();
+        expect(todoRows[0]).toHaveTextContent("My task");
+        expect(screen.queryByText("Extra task")).not.toBeInTheDocument();
+        expect(todoRows[1]).toHaveTextContent("Another task");
+    });
 });
