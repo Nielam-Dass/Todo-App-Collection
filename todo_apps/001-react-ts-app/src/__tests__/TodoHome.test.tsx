f6fadaea0b1910b66c16a7ca26d0771068f5aa1d
Nielam Dass <nielam.dass@gmail.com>
2025-06-13 00:28:43 -0400

    Added test for TodoHome component to ensure all task rows are displayed

--- todo_apps/001-react-ts-app/src/__tests__/TodoHome.test.tsx
+++ todo_apps/001-react-ts-app/src/__tests__/TodoHome.test.tsx
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom/vitest';
 
 import TodoHome from '../TodoHome';
 import { MemoryRouter } from 'react-router-dom';
+import Task from '../Task';
 
 
 describe("Todo home page tests", () => {
@@ -34,5 +35,22 @@ describe("Todo home page tests", () => {
         );
         expect(screen.queryByText("There are no tasks left")).not.toBeInTheDocument();
         expect(screen.queryByRole("table")).toBeInTheDocument();
+    });
+
+    it("Displays table with all tasks", () => {
+        const taskList: Task[] = [
+            {taskId: "id-1", taskName: "Task 1", taskUrgency: 1},
+            {taskId: "id-2", taskName: "Task 2", taskUrgency: 5},
+        ]
+        render(
+            <MemoryRouter>
+                <TodoHome tasks={taskList} addTask={()=>{}} deleteTask={()=>{}}/>
+            </MemoryRouter>
+        );
+        expect(screen.queryByRole("table")).toBeInTheDocument();
+        const todoRows: HTMLElement[] = screen.queryAllByRole("row");
+        expect(todoRows).toHaveLength(3);
+        expect(todoRows[1]).toHaveTextContent("Task 1" + "1");
+        expect(todoRows[2]).toHaveTextContent("Task 2" + "5");
     });
 });
