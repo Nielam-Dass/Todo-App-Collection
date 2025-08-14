89ea275eb9a1d14550b207eb582bbcb14ab8e7ca
Nielam Dass <nielam.dass@gmail.com>
2025-08-14 18:00:44 -0400

    Added rootController test to ensure response has an empty array when there are no tasks in database

--- todo_apps/002-mern-app/backend/tests/rootController.test.js
+++ todo_apps/002-mern-app/backend/tests/rootController.test.js
@@ -3,7 +3,7 @@ const mockingoose = require("mockingoose");
 const Task = require("../src/models/Task");
 
 
-test("Test to get all list of tasks", async () => {
+test("Get array of all tasks", async () => {
     const taskList = [
         {
             taskName: "Task 1",
@@ -31,4 +31,16 @@ test("Test to get all list of tasks", async () => {
     expect(mockResponse.json).toHaveBeenCalledTimes(1);
     expect(mockResponse.json.mock.calls[0][0]).toHaveLength(3);
     expect(mockResponse.json).toHaveBeenLastCalledWith(taskList.map((task) => expect.objectContaining(task)));
+});
+
+test("Get empty array when there are no tasks", async () => {
+    mockingoose(Task).toReturn([], "find");
+    
+    const mockResponse = { json: jest.fn() };
+    expect(mockResponse.json).toHaveBeenCalledTimes(0);
+
+    await rootController.getAllTasks(null, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    expect(mockResponse.json).toHaveBeenLastCalledWith([]);
 });
