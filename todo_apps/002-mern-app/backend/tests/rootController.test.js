781af826975ea22da2b890449c05f11488ddc99f
Nielam Dass <nielam.dass@gmail.com>
2025-08-13 21:16:23 -0400

    Created unit testing for rootController using mockingoose

---
+++ todo_apps/002-mern-app/backend/tests/rootController.test.js
@@ -0,0 +1,34 @@
+const rootController = require("../src/controllers/rootController");
+const mockingoose = require("mockingoose");
+const Task = require("../src/models/Task");
+
+
+test("Test to get all list of tasks", async () => {
+    const taskList = [
+        {
+            taskName: "Task 1",
+            taskDescription: "The first task",
+            taskCompleted: false
+        },
+        {
+            taskName: "Task 2",
+            taskDescription: "The second task",
+            taskCompleted: false
+        },
+        {
+            taskName: "Task 3",
+            taskDescription: "The last task",
+            taskCompleted: true
+        }
+    ];
+    mockingoose(Task).toReturn(taskList, "find");
+
+    const mockResponse = { json: jest.fn() };
+    expect(mockResponse.json).toHaveBeenCalledTimes(0);
+
+    await rootController.getAllTasks(null, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    expect(mockResponse.json.mock.calls[0][0]).toHaveLength(3);
+    expect(mockResponse.json).toHaveBeenLastCalledWith(taskList.map((task) => expect.objectContaining(task)));
+});
