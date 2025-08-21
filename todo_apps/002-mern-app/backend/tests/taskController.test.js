77ac206b8f61a2a3c5bbc2f6423c71ea92791958
Nielam Dass <nielam.dass@gmail.com>
2025-08-21 00:42:34 -0400

    Added taskController test to ensure 404 error is returned when accessing an invalid task id

--- todo_apps/002-mern-app/backend/tests/taskController.test.js
+++ todo_apps/002-mern-app/backend/tests/taskController.test.js
@@ -10,10 +10,10 @@ test("Get individual task by id",  async () => {
         taskName: "My task",
         taskDescription: "My task description",
         taskCompleted: false
-    }
+    };
     mockingoose(Task).toReturn(taskDoc, "findOne");
 
-    requestObj = {
+    const requestObj = {
         params: {
             taskId: taskDoc._id
         }
@@ -31,4 +31,33 @@ test("Get individual task by id",  async () => {
     expect(mockResponse.json).toHaveBeenLastCalledWith(taskDoc);
     expect(mockResponse.status).toHaveBeenCalledTimes(1);
     expect(mockResponse.status).toHaveBeenLastCalledWith(200);
+});
+
+test("Get 404 error for nonexistent task", () => {
+    taskDoc = {
+        _id: new mongoose.Types.ObjectId(),
+        taskName: "My task",
+        taskDescription: "My task description",
+        taskCompleted: false
+    };
+    mockingoose(Task).toReturn(taskDoc, "findOne");
+
+    const requestObj = {
+        params: {
+            taskId: "1234"
+        }
+    };
+    const mockResponse = { 
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+    };
+    expect(mockResponse.json).toHaveBeenCalledTimes(0);
+    expect(mockResponse.status).toHaveBeenCalledTimes(0);
+
+    taskController.getTaskInfo(requestObj, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    expect(mockResponse.json).toHaveBeenLastCalledWith({message: "Task not found"});
+    expect(mockResponse.status).toHaveBeenCalledTimes(1);
+    expect(mockResponse.status).toHaveBeenLastCalledWith(404);
 });
