b792c0868a62e189223111cb8535f349cb5b7882
Nielam Dass <nielam.dass@gmail.com>
2025-08-20 23:06:00 -0400

    Added taskController test file

---
+++ todo_apps/002-mern-app/backend/tests/taskController.test.js
@@ -0,0 +1,34 @@
+const taskController = require("../src/controllers/taskController");
+const mockingoose = require("mockingoose");
+const Task = require("../src/models/Task");
+const mongoose = require("mongoose");
+
+
+test("Get individual task by id",  async () => {
+    taskDoc = {
+        _id: new mongoose.Types.ObjectId(),
+        taskName: "My task",
+        taskDescription: "My task description",
+        taskCompleted: false
+    }
+    mockingoose(Task).toReturn(taskDoc, "findOne");
+
+    requestObj = {
+        params: {
+            taskId: taskDoc._id
+        }
+    };
+    const mockResponse = { 
+        json: jest.fn(),
+        status: jest.fn().mockReturnThis()
+    };
+    expect(mockResponse.json).toHaveBeenCalledTimes(0);
+    expect(mockResponse.status).toHaveBeenCalledTimes(0);
+
+    await taskController.getTaskInfo(requestObj, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledTimes(1);
+    expect(mockResponse.json).toHaveBeenLastCalledWith(taskDoc);
+    expect(mockResponse.status).toHaveBeenCalledTimes(1);
+    expect(mockResponse.status).toHaveBeenLastCalledWith(200);
+});
