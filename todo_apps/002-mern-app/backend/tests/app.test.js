554f08c55f8cf76873712e7ba039406ef142860d
Nielam Dass <nielam.dass@gmail.com>
2025-09-17 01:54:40 -0400

    Added integration test to ensure 404 is returned for GET request to task route

--- todo_apps/002-mern-app/backend/tests/app.test.js
+++ todo_apps/002-mern-app/backend/tests/app.test.js
@@ -6,3 +6,8 @@ test("Get 200 OK status at root page", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
 });
+
+test("Get 404 error when sending GET request to task route", async () => {
+    const response = await request(app).get("/task");
+    expect(response.status).toBe(404);
+});
