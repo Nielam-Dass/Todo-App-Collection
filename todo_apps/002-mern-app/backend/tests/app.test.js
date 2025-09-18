55628b5cfd983832469ac850a30fc180d30b94d2
Nielam Dass <nielam.dass@gmail.com>
2025-09-17 22:41:15 -0400

    Modified app integration tests to check response type and body

--- todo_apps/002-mern-app/backend/tests/app.test.js
+++ todo_apps/002-mern-app/backend/tests/app.test.js
@@ -5,9 +5,12 @@ const request = require("supertest");
 test("Get 200 OK status at root page", async () => {
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
 });
 
 test("Get 404 error when sending GET request to task route", async () => {
     const response = await request(app).get("/task");
     expect(response.status).toBe(404);
+    expect(response.type).toBe("application/json");
+    expect(response.body).toEqual({ message: "404 Not Found" });  // toEqual() checks for deep equality of objects, unlike toBe()
 });
