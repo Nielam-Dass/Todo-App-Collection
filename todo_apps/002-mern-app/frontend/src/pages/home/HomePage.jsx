1aa9036e70a370ca87ba8ecff85f170c6d69cea5
Nielam Dass <nielam.dass@gmail.com>
2025-12-30 22:43:42 -0500

    Using useQuery to fetch tasks on home page

--- todo_apps/002-mern-app/frontend/src/pages/home/HomePage.jsx
+++ todo_apps/002-mern-app/frontend/src/pages/home/HomePage.jsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllTasks } from "../../services/taskService";
+
+
 function HomePage () {
+  const query = useQuery({
+    queryKey: ["tasks"],
+    queryFn: fetchAllTasks
+  });
+  console.log(query.data);
+  
   return (
     <div>Home - TODO Root Route</div>
   );
