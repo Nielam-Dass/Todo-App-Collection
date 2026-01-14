12fa41575a21c932ff196e0a36b7eebc136d542a
Nielam Dass <nielam.dass@gmail.com>
2026-01-14 00:32:43 -0500

    Added checkbox and delete button to TaskCard component

--- todo_apps/002-mern-app/frontend/src/pages/home/TaskCard.jsx
+++ todo_apps/002-mern-app/frontend/src/pages/home/TaskCard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import trashCanIcon from "../../assets/trash_can.svg";
 
 
 function TaskCard(props) {
@@ -9,10 +10,14 @@ function TaskCard(props) {
   }
   
   return (
-    <div style={{border: "1px solid black", borderRadius: "10px", padding: "20px 40px", fontSize: "2rem"}}
+    <div style={{border: "1px solid black", borderRadius: "10px", padding: "20px 40px", fontSize: "2rem", display: "flex", alignItems: "center", gap: "8px"}}
       onClick={()=>handleTaskClick(props.task._id)}
     >
-      {props.task.taskName}
+      <input type="checkbox" onClick={(e)=>e.stopPropagation()} style={{width: "2rem", height: "2rem"}}/>
+      <span style={{flexGrow: 1}}>{props.task.taskName}</span>
+      <button style={{padding: "4px"}} onClick={(e)=>e.stopPropagation()}>
+        <img src={trashCanIcon} width={"28px"} height={"28px"}/>
+      </button>
     </div>
   );
 }
