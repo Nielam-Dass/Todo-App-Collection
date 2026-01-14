98bd0b4d52546f0f4fd2b8a2ba424345d5314e49
Nielam Dass <nielam.dass@gmail.com>
2026-01-13 20:40:47 -0500

    Navigate to individual task route when task card is clicked

--- todo_apps/002-mern-app/frontend/src/pages/home/TaskCard.jsx
+++ todo_apps/002-mern-app/frontend/src/pages/home/TaskCard.jsx
@@ -1,6 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
+
 function TaskCard(props) {
+  const navigate = useNavigate();
+
+  const handleTaskClick = (taskId) => {
+    navigate(`/task/${taskId}`);
+  }
+  
   return (
-    <div style={{border: "1px solid black", borderRadius: "10px", padding: "20px 40px", fontSize: "2rem"}}>{props.task.taskName}</div>
+    <div style={{border: "1px solid black", borderRadius: "10px", padding: "20px 40px", fontSize: "2rem"}}
+      onClick={()=>handleTaskClick(props.task._id)}
+    >
+      {props.task.taskName}
+    </div>
   );
 }
 
