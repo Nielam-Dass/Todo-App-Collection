81a48012a6d8aec09b5ed0cabc6daf5aed4b5564
Nielam Dass <nielam.dass@gmail.com>
2026-01-22 00:40:24 -0500

    Defined list of mocked tasks as a global variable

--- todo_apps/002-mern-app/frontend/src/services/taskService.js
+++ todo_apps/002-mern-app/frontend/src/services/taskService.js
@@ -5,33 +5,36 @@ const delay = (delayTime) => {
     });
 }
 
+// List of fake tasks
+const mockTasks = [
+    {
+        _id: "1",
+        taskName: "First Task",
+        taskDescription: "My first task",
+        taskCompleted: false,
+    },
+    {
+        _id: "2",
+        taskName: "Second Task",
+        taskDescription: "My second task",
+        taskCompleted: false,
+    },
+    {
+        _id: "3",
+        taskName: "Third Task",
+        taskDescription: "My third task",
+        taskCompleted: true,
+    },
+    {
+        _id: "4",
+        taskName: "Fourth Task",
+        taskDescription: "My fourth task",
+        taskCompleted: false,
+    }
+];
+
 // TODO: Query backend to fetch all tasks
 export const fetchAllTasks = async () => {
     await delay(2000);
-    return [
-        {
-            _id: "1",
-            taskName: "First Task",
-            taskDescription: "My first task",
-            taskCompleted: false,
-        },
-        {
-            _id: "2",
-            taskName: "Second Task",
-            taskDescription: "My second task",
-            taskCompleted: false,
-        },
-        {
-            _id: "3",
-            taskName: "Third Task",
-            taskDescription: "My third task",
-            taskCompleted: true,
-        },
-        {
-            _id: "4",
-            taskName: "Fourth Task",
-            taskDescription: "My fourth task",
-            taskCompleted: false,
-        }
-    ];
+    return mockTasks;
 }
